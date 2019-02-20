2ab4ed935e367e57fa18b16426a74ee5c9616c72
Andrew <booleanorama@gmail.com>
2019-02-20 21:31:54 +0000

    detail view added

--- dgirls/dgirls/urls.py
+++ dgirls/dgirls/urls.py
@@ -13,22 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-# Use include() to add paths from the catalog application
-from django.urls import include
-from django.urls import path
-urlpatterns += [
     path('blog/', include('blog.urls')),
-]
-
-#Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='/blog/')),
 ]
