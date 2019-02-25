967a8e5cfb7c0e6c786cf38ae9fe0288a10a68eb
Andrew <booleanorama@gmail.com>
2019-02-25 19:00:58 +0000

    Add comment function

--- dgirls/blog/urls.py
+++ dgirls/blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
 	  path('post/<pk>/delete/', views.post_delete, name='post_delete'),
 	  path('drafts/', views.post_draft_list, name='post_draft_list'),
 	  path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+	  path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+       	  path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+          path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
